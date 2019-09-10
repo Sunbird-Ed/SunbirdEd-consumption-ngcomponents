85c25f6df57c42fd119b78662c5a32a6abe80f80
Vivek Kasture <vivek_kasture@techjoomla.com>
2019-09-10 13:01:50 +0530

    Issue #0000 feat: Initial Library setup

---
+++ projects/consumption-components/src/lib/card/library-card/library-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'sb-library-card',
+  templateUrl: './library-card.component.html',
+  styleUrls: ['./library-card.component.scss']
+})
+export class LibraryCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
