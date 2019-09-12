69a85b27be348b5473fec4d219dc9cb78d2c277d
Vivek Kasture <vivek_kasture@techjoomla.com>
2019-09-12 14:46:30 +0530

    Issue SB-13915 feat: Library card implementation

--- projects/consumption-components/src/lib/card/library-card/library-card.component.ts
+++ projects/consumption-components/src/lib/card/library-card/library-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { staticContent } from './library-card.data';
 
 @Component({
   selector: 'sb-library-card',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LibraryCardComponent implements OnInit {
 
-  constructor() { }
+  @Input() content = staticContent;
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
