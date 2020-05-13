3f5e29e3d229d1b18339a65514dd3bf6b9fc23e6
sharath <sharaths.kashyap@gmail.com>
2020-05-13 15:45:47 +0530

    Issue #SB-0000 Chore:Member Card

--- projects/common-consumption/src/lib/card/member-card/member-card.component.ts
+++ projects/common-consumption/src/lib/card/member-card/member-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ICardClick } from '../models';
 
 @Component({
   selector: 'sb-member-card',
@@ -10,19 +11,36 @@ export class MemberCardComponent implements OnInit {
   paletteList:any = ["#FE0030","#7B1FA2","#0097A7","#7B1FA2",
   "#F57C00","#1976D2","#CDDC39","#FE0030","#F57C00","#388E3C"];
 
+  @Input() title:string;
+  @Input() isAdmin: boolean;
+  @Input() isMenu: boolean;
+  @Input() menuOptions: string[];
+  @Input() indexOfMember: number;
+  @Input() initial: number;
+  @Input() identifier: string;
+
+  @Output() menuClick: EventEmitter<ICardClick> = new EventEmitter();
+  @Output() cardClick: EventEmitter<ICardClick> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  fetchStyle(index:number) {
-    if(index > 10) {
-      index = index % 10;
+  fetchStyle() {
+    if(this.indexOfMember > 10) {
+      this.indexOfMember = this.indexOfMember % 10;
     }
     return {
-      'border-color': this.paletteList[index],
-      'color': this.paletteList[index]
+      'border-color': this.paletteList[this.indexOfMember],
+      'color': this.paletteList[this.indexOfMember]
     };
   }
+  onClick(event: MouseEvent) {
+    this.cardClick.emit({ event: event, data: {title: this.title} });
+ }
+ onMenuClick(event: MouseEvent) {
+  this.menuClick.emit({ event: event, data: {identifier:this.identifier} });
+ }
 
 }
