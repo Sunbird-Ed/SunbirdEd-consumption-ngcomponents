bf744210263681d2da04af56c222ab294f74e615
swayangjit <swayangjit029@gmail.com>
2020-07-09 11:14:41 +0530

    Issue #SB-000 fix: Updated karma config.

--- projects/common-consumption/karma.conf.js
+++ projects/common-consumption/karma.conf.js
@@ -26,6 +26,14 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false
+    singleRun: false,
+    browserDisconnectTimeout: 10000,
+    browserDisconnectTolerance: 3,
+    browserNoActivityTimeout: 60000,
+    flags: [
+      '--disable-web-security',
+      '--disable-gpu',
+      '--no-sandbox'
+    ]
   });
 };
