2734efbcd32f3ab236c768429f7e595d82b6be87
Vivek Kasture <vivek_kasture@techjoomla.com>
2020-08-15 22:31:14 +0530

    Issue #SH-0000 feat: Updated certificate actions

--- projects/common-consumption/src/lib/certificate/certificate-actions/certificate-actions/certificate-actions.component.ts
+++ projects/common-consumption/src/lib/certificate/certificate-actions/certificate-actions/certificate-actions.component.ts
@@ -1,35 +1,35 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+export interface IPreviewActionButton {
+  label: string;
+  name: string;
+  show: boolean;
+}
+
+export interface IPreviewActionButtonClick {
+  event: MouseEvent;
+  name: string;
+}
 
 @Component({
   selector: 'sb-certificate-actions',
   templateUrl: './certificate-actions.component.html',
   styleUrls: ['./certificate-actions.component.scss']
 })
-export class CertificateActionsComponent implements OnInit {
-  @Input() select = { show: false, label: 'Select' };
-  @Input() preview = { show: false, label: 'Preview' };
-  @Input() remove = { show: false, label: 'Remove' };
+
+export class CertificateActionsComponent {
+  @Input() select: IPreviewActionButton = { show: false, label: 'Select', name: 'select' };
+  @Input() preview: IPreviewActionButton = { show: false, label: 'Preview', name: 'preview' };
+  @Input() remove: IPreviewActionButton = { show: false, label: 'Remove', name: 'remove' };
   @Input() template: string;
   @Input() isSelectedTemplate = false;
 
-  @Output() selectClick = new EventEmitter<MouseEvent>();
-  @Output() previewClick = new EventEmitter<MouseEvent>();
-  @Output() removeClick = new EventEmitter<MouseEvent>();
+  @Input() actionButtons: IPreviewActionButton[] = [];
+  @Output() buttonClick = new EventEmitter<IPreviewActionButtonClick>();
   constructor() { }
 
-  ngOnInit() {
-  }
-
-  onSelectClick(event: MouseEvent) {
-    this.selectClick.emit(event);
-  }
-
-  onPreviewClick(event: MouseEvent) {
-    this.previewClick.emit(event);
-  }
-
-  onRemoveClick(event: MouseEvent) {
-    this.removeClick.emit(event);
+  onButtonClick(event: MouseEvent, name: string) {
+    this.buttonClick.emit({ event, name });
   }
 
 }
