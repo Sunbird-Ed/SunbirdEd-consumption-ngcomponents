a47dcaa0d613f1ab861b8a91e3bae92a47c4c1a9
Ajoymaity <ajoy.maity@tarento.com>
2020-11-09 13:35:03 +0530

    Issue #SB-21321 fix: Fixed Expanded view of the unit for textbook filters

--- projects/common-consumption/src/lib/card/models.ts
+++ projects/common-consumption/src/lib/card/models.ts
@@ -201,3 +201,16 @@ export enum PlatformType {
     PORTAL = 'portal',
     MOBILE = 'mobile'
 }
+
+export enum ExpandMode {
+    SINGLE = 'single',
+    MULTIPLE = 'multiple'
+}
+export enum ExpandBehavior {
+    NONE = 'none',
+    EXPAND_FIRST = 'expand-first'
+}
+export interface IAccordianConfig {
+    expandMode: ExpandMode;
+    expandBehavior: ExpandBehavior;
+}
