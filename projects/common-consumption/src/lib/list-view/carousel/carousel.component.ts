f1d1f5d5bc89ce30276ab106a905184c639a44d7
Naveen Kumar V <naveenkumar.vamd@gmail.com>
2021-04-07 13:41:47 +0530

    Issue #SB-22743 feat: modifying carousel components

--- projects/common-consumption/src/lib/list-view/carousel/carousel.component.ts
+++ projects/common-consumption/src/lib/list-view/carousel/carousel.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, Output, EventEmitter, ViewChild, TemplateRef } from '@angular/core';
-import { List } from '../models';
+import { Component, Input, Output, EventEmitter, ViewChild, ElementRef } from '@angular/core';
 import { ListData } from './carousel.data';
 
 @Component({
@@ -12,11 +11,29 @@ export class CarouselComponent {
 
     @Input() title?: string;
     @Input() subTitle?: string;
-    @Input() listData: List[] = ListData;s
+    @Input() listData = ListData;
+    @ViewChild('carouselContainer', { read: ElementRef, static: false }) public gallery: ElementRef<any>;
 
     @Output() itemClick: EventEmitter<any> = new EventEmitter();
 
     selectedIndex = -1;
+    galleryScroller: any;
+    galleryItemSize: any;
+
+    ngOnInit(): void {
+        setTimeout(() => {
+        this.galleryScroller = this.gallery.nativeElement.querySelector('.carousel-scroller');
+        this.galleryItemSize = this.galleryScroller.querySelector('div').clientWidth;
+        }, 0);
+    }
+
+    scrollToNextPage() {
+        this.galleryScroller.scrollBy(this.galleryItemSize, 0);
+    }
+
+    scrollToPrevPage() {
+        this.galleryScroller.scrollBy(-this.galleryItemSize, 0);
+    }
 
     onItemSelect(event, item, index) {
         this.selectedIndex = index;
--- projects/common-consumption/src/lib/list-view/carousel/carousel.data.ts
+++ projects/common-consumption/src/lib/list-view/carousel/carousel.data.ts
@@ -1,27 +1,52 @@
 export const ListData = [
   {
     label: 'title 1',
-    image: 'assets/common-consumption/images/completed.svg',
+    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
     data: {}
   },
   {
     label: 'title 2',
-    image: 'assets/common-consumption/images/completed.svg',
+    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
     data: {}
   },
   {
     label: 'title 3',
-    image: 'assets/common-consumption/images/completed.svg',
+    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
     data: {}
   },
   {
     label: 'title 4',
-    image: 'assets/common-consumption/images/completed.svg',
+    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
     data: {}
   },
   {
     label: 'title 5',
-    image: 'assets/common-consumption/images/completed.svg',
+    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
+    data: {}
+  },
+  {
+    label: 'title 6',
+    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
+    data: {}
+  },
+  {
+    label: 'title 7',
+    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
+    data: {}
+  },
+  {
+    label: 'title 8',
+    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
+    data: {}
+  },
+  {
+    label: 'title 9',
+    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
+    data: {}
+  },
+  {
+    label: 'title 10',
+    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
     data: {}
   }  
 ];
