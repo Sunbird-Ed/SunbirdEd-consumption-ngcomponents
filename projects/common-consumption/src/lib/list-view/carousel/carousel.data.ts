fa79c585b07ac03189500a27cc47ef3197cef6c5
Naveen Kumar V <naveenkumar.vamd@gmail.com>
2021-04-26 16:59:23 +0530

    Issue #SB-22743 feat: fixing video UI issue

--- projects/common-consumption/src/lib/list-view/carousel/carousel.data.ts
+++ projects/common-consumption/src/lib/list-view/carousel/carousel.data.ts
@@ -1,52 +1,30 @@
 export const ListData = [
   {
-    label: 'title 1',
-    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
+    id: "1",
+    name: "Sample title 1",
+    thumbnail: "https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/do_31309320735055872011111/artifact/nishtha_icon.thumb.jpg",
+    url: "https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/assets/do_31309320735055872011111/engagement-with-language-.mp4",
     data: {}
   },
   {
-    label: 'title 2',
-    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
+    id: "2",
+    name: "Sample title 2",
+    thumbnail: "https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/do_31309320735055872011111/artifact/nishtha_icon.thumb.jpg",
+    url: "https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/assets/do_31309320735055872011111/engagement-with-language-.mp4",
     data: {}
   },
   {
-    label: 'title 3',
-    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
+    id: "3",
+    name: "Sample title 3",
+    thumbnail: "https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/do_31309320735055872011111/artifact/nishtha_icon.thumb.jpg",
+    url: "https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/assets/do_31309320735055872011111/engagement-with-language-.mp4",
     data: {}
   },
   {
-    label: 'title 4',
-    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
+    id: "4",
+    name: "Sample title 4",
+    thumbnail: "https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/do_31309320735055872011111/artifact/nishtha_icon.thumb.jpg",
+    url: "https://ntpproductionall.blob.core.windows.net/ntp-content-production/content/assets/do_31309320735055872011111/engagement-with-language-.mp4",
     data: {}
   },
-  {
-    label: 'title 5',
-    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
-    data: {}
-  },
-  {
-    label: 'title 6',
-    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
-    data: {}
-  },
-  {
-    label: 'title 7',
-    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
-    data: {}
-  },
-  {
-    label: 'title 8',
-    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
-    data: {}
-  },
-  {
-    label: 'title 9',
-    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
-    data: {}
-  },
-  {
-    label: 'title 10',
-    image: 'https://ntpstagingall.blob.core.windows.net/ntp-content-staging/content/do_2127645021030563841271/artifact/download_1557833776003.thumb.jpg',
-    data: {}
-  }  
 ];
